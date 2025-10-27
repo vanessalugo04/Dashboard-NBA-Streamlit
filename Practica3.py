f18479f8ea8e5fb146e12f89431398581c266184
Vanessa <vlugoavalos@gmail.com>
2025-10-27 09:51:34 -0600

    filtros

--- Practica3.py
+++ Practica3.py
@@ -2,6 +2,17 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('nba_all_elo.csv')
-st.line_chart(df)
 
-add_selecbox = st.sidebar.selectbox('Seleccionar año', (df['year_id']).unique())
+st.sidebar.header('Filtros')
+
+year_list = df['year_id'].unique()
+team_list = df['team_id'].unique()
+tipo_list = df['is_playoffs'].unique()
+
+opciones = ['regular', 'plpayoffs', 'ambos']
+
+with st.sidebar:
+    year = st.selectbox('Seleccionar año', year_list, 0)
+    team = st.selectbox('Seleccionar equipo', team_list, 0)
+    tipo = st.pills('Seleccionar tipo', opciones)
+ 
